911ef9fe807760db9d98d60d574801278952d4d3
Ranu <ranu.aggarwal@comprotechnologies.com>
2019-12-19 11:08:26 +0530

    Updates

---
+++ precache-manifest.45a57458fbe120f8850bbe6f1f142b8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7fb4bc5b502dfd7ad60f8ea16f1e5d",
+    "url": "/my-app2/index.html"
+  },
+  {
+    "revision": "9d8e318e15f261741b9a",
+    "url": "/my-app2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9422195bee5922838cc7",
+    "url": "/my-app2/static/js/2.1f8adf6b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app2/static/js/2.1f8adf6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d8e318e15f261741b9a",
+    "url": "/my-app2/static/js/main.f7180d14.chunk.js"
+  },
+  {
+    "revision": "b927a0edb2b314bee895",
+    "url": "/my-app2/static/js/runtime-main.6a4ecce4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app2/static/media/logo.5d5d9eef.svg"
+  }
+]);
